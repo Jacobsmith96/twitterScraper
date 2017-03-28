d8845d841207b54563beb52e262a3e0ee875b4bc
JacobSmith96 <jacobysmith96@gmail.com>
2017-03-28 11:15:01 -0400

    Fully implemented the user scrape with comments

--- userScrape/analyzeUser.py
+++ userScrape/analyzeUser.py
@@ -1,9 +1,12 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-#Define a function to count the number of words in each tweet
+
+#Open the files of positive and negative words
 positives = open('positive.txt', 'r').read().split('\n')
 negatives = open('negative.txt', 'r').read().split('\n')
+
+#Define a function to count the number of words in each tweet
 def countWords(x):
     if x is not None:
         split = x.split(' ')
@@ -15,6 +18,7 @@ def countWords(x):
     else:
         return 0
 
+#Define a function that removes unnecessary characters from tweets
 def cleanString(x):
     if x is not None:
         x = x.lower()
@@ -46,35 +50,46 @@ def countNegative(x):
                 count = count + 1
     return count    
 
+#The main function
 def main():
     print "Reading Tweets\n"
 
+    #Define the path of the output file
     tweets_data_path = 'twitter_data.txt'
-
+    
+    #Create a list for converting the json strings into a datatable format
     tweets_data = []
+    #Open the data file for reading
     tweets_file = open(tweets_data_path, "r")
+    #Iterate over the lines in the tweet file
     for line in tweets_file:
         try:
+            #Append these json blobs into the tweets_data list
             tweet = json.loads(line)
             tweets_data.append(tweet)
         except:
             continue
+    #Output the number of tweets in the list
     print("Number of Tweets: " + str(len(tweets_data)))
     print "Structuring Tweets\n"
+
+    #Create an empty pandas DataFrame for splitting up the tweets
     tweets = pd.DataFrame()
     
 
+    #Perform a series of mappings that create new columns in the pandas DataFrame
     tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
     tweets['cleanText'] = tweets['text'].map(cleanString)
     tweets['numWords'] = tweets['cleanText'].map(countWords)
     tweets['numPos'] = tweets['cleanText'].map(countPositive)
     tweets['numNeg'] = tweets['cleanText'].map(countNegative)
     tweets['sent'] = tweets['numPos']-tweets['numNeg']
+
+    #Output findings
     print("Mean positive sentiment words: " + str((tweets['numPos']).mean()))
     print("Mean negtave sentiment words: " + str((tweets['numNeg']).mean()))
     print("Mean combined sentiment: " + str((tweets['sent']).mean()))
-    for tweet in tweets['cleanText']:
-        print tweet
+    
 
 if __name__ == '__main__':
     main()
--- userScrape/userScrape.py
+++ userScrape/userScrape.py
@@ -1,34 +1,68 @@
 import tweepy
 from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-from tweepy import Stream
 import pandas
 import sys
 import json
+
+#Setup twitter authentication 
 consumer_key = "VzEgtLw9mhYA1gecaOLTS7US6"
 consumer_secret = "AwMCrGIpjNfC1DBPjgIuONH57v2ueJLK1ovIOIwLjyVwsEFhJK"
 access_token = "844933048911237121-oJZ9iTf9J4Sse4S7XmfPudc5WSdgRQr"
 access_token_secret = "TqEt9xmbPnPXZxYvorEJOT2GStBL4Hzw0msVCyeMuBx9G"
 
+#Function that runs when a Twitter username is provided
 def main(screen_name):
+     #Finish setting up authentication and creating the Tweepy API object
      auth = OAuthHandler(consumer_key, consumer_secret)
      auth.set_access_token(access_token, access_token_secret)
      api = tweepy.API(auth)
      
+     #Open the file to write the json blobs of Twitter Statuses to
      out = open("twitter_data.txt", "w") 
+
+     #Create variables for tracking total tweets processed
+     totalLen = 0
+
+     #Get the first batch of tweets from the user
+     new_tweets = api.user_timeline(screen_name = screen_name, count=200)
      
-     new_tweets = api.user_timeline(screen_name = screen_name, count=200)
-
-     #while len(new_tweets) > 0:       
-     new_tweets = api.user_timeline(screen_name = screen_name, count=200)
+     #Iterate over the first batch of tweets, writing non-retweets to the output file
      for x in new_tweets:
-        print(x._json['id'])
-        out.write(json.dumps(x._json))
-        out.write("\n")
+         if 'retweeted_status' not in x._json:
+                out.write(json.dumps(x._json))
+                out.write("\n")
+       
+     #Get the id of the oldest tweet, used to ensure no duplicates in future API calls
+     oldest = new_tweets[-1].id
+   
+     #Increase the total length counter
+     totalLen = totalLen + len(new_tweets)
 
+     #Continue to loop over tweets while there a still new ones being retrieved
+     #Currently can only loop 15 times, need to add a timer to get around the API restrictions
+     while len(new_tweets) > 0: 
+         print "getting tweets before %s" %(oldest)
+
+         #Get a new batch of tweets using the previous oldest id ad max_id
+         new_tweets = api.user_timeline(screen_name = screen_name, count = 200,  max_id=oldest-1)
+         #Increment the total length variable
+         totalLen = totalLen + len(new_tweets)
+         
+         #Set the new oldest id
+         oldest = new_tweets[-1].id
+         
+         #Handle output of Tweets to the output file
+         for x in new_tweets:
+             if 'retweeted_status' not in x._json:
+                out.write(json.dumps(x._json))
+                out.write("\n")
+        
+         print "...%s tweets downloaded so far" % (totalLen)
 if __name__ == '__main__':
+    #Make sure the user inputs only one input after the file name 
     if len(sys.argv) <2 or len(sys.argv)>2:
         print("Invalid number of arguments (should be of the format python userScrape.py <screen_name>)")
     else:
+        #Run the main function
         main(sys.argv[1]) 
         
